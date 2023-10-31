cb5fe494a1885d78af692911fed5726282ef59a1
netanelfrankel <netanel.frankel@fiserv.com>
2023-10-31 14:42:17 -0400

    ty joshua

--- Solution.java
+++ Solution.java
@@ -1,12 +1,60 @@
+import java.util.HashMap;
+
 class Solution {
-    public static char mostFrequentChar(String s) {
-      
-      return;
+//    public static char mostFrequentChar (String s) {
+//
+//      if(s.isBlank()){return ' ';}
+//
+//      HashMap<Character,Integer> counter = new HashMap<>();
+//      char mostFreqChar = s.charAt(0);
+//
+//      int mostFreqTime = 1;
+//
+//      for(char c: s.toCharArray()){
+//          counter.put(c,(counter.getOrDefault(c,0))+1);
+//
+//          if(counter.get(c)>mostFreqTime){
+//              mostFreqChar = c;
+//              mostFreqTime = counter.get(c);
+//          }
+//
+//          else if(counter.get(c).equals(counter.get(mostFreqChar))){
+//              mostFreqChar = s.charAt(Math.min(s.indexOf(c),s.indexOf(mostFreqChar)));
+//          }
+//      }
+//
+//
+//      return mostFreqChar;
+//    }
+
+    public static char mostFrequentChar (String s) {
+
+        if(s.isBlank()){return ' ';}
+
+        HashMap<Character,Integer> counter = new HashMap<>();
+        char mostFreqChar = s.charAt(0);
+
+        int mostFreqTime = 1;
+
+        for(char c: s.toCharArray()){
+            counter.put(c,(counter.getOrDefault(c,0))+1);
+
+            if(counter.get(c)>=mostFreqTime){
+                mostFreqChar = c;
+                mostFreqTime = counter.get(c);
+            }
+
+
+        }
+
+
+        return mostFreqChar;
     }
   
     public static void main(String[] args) {
       // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("boookeeper")); // -> 'e'
+        System.out.println(Solution.mostFrequentChar("mississippi")); // -> 'i'
     }
 
   }
